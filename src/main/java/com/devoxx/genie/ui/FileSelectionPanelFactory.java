3f9e062c7d7df830c0cdface1918dc64f5fc5d20
Stephan Janssen <sja@devoxx.com>
2024-04-26 18:04:11 +0200

    Removed project filter for now

--- src/main/java/com/devoxx/genie/ui/FileSelectionPanelFactory.java
+++ src/main/java/com/devoxx/genie/ui/FileSelectionPanelFactory.java
@@ -8,23 +8,17 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.messages.MessageBus;
 import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.Collection;
-
-import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
 
 /**
  * The factory class for creating a panel with a text field for filtering files and a list of open files
@@ -44,29 +38,6 @@ public class FileSelectionPanelFactory implements DumbAware {
     public static JPanel createPanel(Project project) {
         JBList<VirtualFile> resultList = createResultList(project);
         JPanel mainPanel = new JPanel(new BorderLayout());
-        PlaceholderTextArea filterInputField = new PlaceholderTextArea();
-        filterInputField.setPlaceholder("Filter files by name");
-        filterInputField.setPreferredSize(new Dimension(0, 30));
-        filterInputField.getDocument().addDocumentListener(new AbstractDocumentListener() {
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                UIUtil.invokeAndWaitIfNeeded((Runnable) () -> {
-                    String filter = filterInputField.getText().trim().toLowerCase();
-                    if (filter.isEmpty() || filter.length() < 3) {
-                        return;
-                    }
-                    PartialNameFileFinderUtil partialNameFileFinderUtil = new PartialNameFileFinderUtil();
-                    Collection<VirtualFile> filteredFiles = partialNameFileFinderUtil.findAndProcessFilesByPartName(project, filter);
-                    DefaultListModel<VirtualFile> listModel = (DefaultListModel<VirtualFile>) resultList.getModel();
-                    listModel.clear();
-                    for (VirtualFile file : filteredFiles) {
-                        listModel.addElement(file);
-                    }
-                });
-            }
-        });
-
-        mainPanel.add(filterInputField, BorderLayout.NORTH);
         mainPanel.add(new JBScrollPane(resultList), BorderLayout.CENTER);
         return mainPanel;
     }
