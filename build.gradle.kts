c3d5eb9a8b384fc6e094affdc451c99e16c2e436
Stephan Janssen <sja@devoxx.com>
2024-05-04 10:41:52 +0200

    Fix: Double click files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.devoxx.genie"
-version = "0.1.1"
+version = "0.1.2"
 
 repositories {
     mavenCentral()
--- src/main/java/com/devoxx/genie/ui/topic/AppTopics.java
+++ src/main/java/com/devoxx/genie/ui/topic/AppTopics.java
@@ -1,6 +1,5 @@
 package com.devoxx.genie.ui.topic;
 
-import com.devoxx.genie.ui.listener.FileSelectionListener;
 import com.devoxx.genie.ui.listener.SettingsChangeListener;
 import com.intellij.util.messages.Topic;
 
@@ -8,7 +7,4 @@ public class AppTopics {
 
     public static final Topic<SettingsChangeListener> SETTINGS_CHANGED_TOPIC =
         Topic.create("SettingsChanged", SettingsChangeListener.class);
-
-    public static final Topic<FileSelectionListener> FILE_SELECTION_TOPIC =
-        Topic.create("File Selection Topic", FileSelectionListener.class);
 }
