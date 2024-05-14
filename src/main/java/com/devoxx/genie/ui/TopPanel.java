549d7d5091800015a88b33318f9c883544dae654
Stephan Janssen <sja@devoxx.com>
2024-05-14 12:49:51 +0200

    First refactorings

---
+++ src/main/java/com/devoxx/genie/ui/TopPanel.java
@@ -0,0 +1,12 @@
+package com.devoxx.genie.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TopPanel extends JPanel {
+
+    public TopPanel() {
+        setLayout(new BorderLayout());
+        // add components
+    }
+}
