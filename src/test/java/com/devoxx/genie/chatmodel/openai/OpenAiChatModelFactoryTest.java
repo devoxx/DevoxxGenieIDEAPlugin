41fb309b0e90e695739a95af1eb84b9276c8c525
Stephan Janssen <sja@devoxx.com>
2024-05-21 15:28:30 +0200

    Tests cleanup

--- src/test/java/com/devoxx/genie/chatmodel/openai/OpenAiChatModelFactoryTest.java
+++ src/test/java/com/devoxx/genie/chatmodel/openai/OpenAiChatModelFactoryTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class JanChatModelFactoryTest extends AbstractLightPlatformTestCase {
+public class OpenAiChatModelFactoryTest extends AbstractLightPlatformTestCase {
 
     @BeforeEach
     public void setUp() throws Exception {
