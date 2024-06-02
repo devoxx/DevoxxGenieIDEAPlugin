4d3261509c10e0bb3254499eacdf71a18f4b6afd
Stephan Janssen <sja@devoxx.com>
2024-06-02 11:02:51 +0200

    Feat #94: Refactoring settings as prep for FormBuilder

--- src/main/java/com/devoxx/genie/ui/util/SettingsDialogUtil.java
+++ src/main/java/com/devoxx/genie/ui/util/SettingsDialogUtil.java
@@ -9,6 +9,6 @@ public class SettingsDialogUtil {
      * Show the settings dialog.
      */
     public static void showSettingsDialog(Project project) {
-        ShowSettingsUtil.getInstance().showSettingsDialog(project, "Devoxx Genie Settings");
+        ShowSettingsUtil.getInstance().showSettingsDialog(project, "DevoxxGenie");
     }
 }
