e20d3d5987995b31bb9472abbff99a7e88897133
Stephan Janssen <sja@devoxx.com>
2024-07-01 21:34:02 +0200

    Fix #131 Actions are now DumbAwareAction

--- src/main/java/com/devoxx/genie/action/AddFileAction.java
+++ src/main/java/com/devoxx/genie/action/AddFileAction.java
@@ -2,22 +2,18 @@ package com.devoxx.genie.action;
 
 import com.devoxx.genie.service.FileListManager;
 import com.devoxx.genie.ui.util.NotificationUtil;
-import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import static com.devoxx.genie.ui.util.WindowPluginUtil.ensureToolWindowVisible;
 
-public class AddFileAction extends AnAction {
+public class AddFileAction extends DumbAwareAction {
 
-    /**
-     * Add file to the window context.
-     *
-     * @param e the action event
-     */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
@@ -29,10 +25,28 @@ public class AddFileAction extends AnAction {
 
         FileListManager fileListManager = FileListManager.getInstance();
         VirtualFile selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-        if (selectedFile != null && !fileListManager.contains(selectedFile)) {
+        if (selectedFile != null && !selectedFile.isDirectory() && !fileListManager.contains(selectedFile)) {
             fileListManager.addFile(selectedFile);
+        } else if (selectedFile != null && selectedFile.isDirectory()) {
+            NotificationUtil.sendNotification(project, "Cannot add directories. Please select a file.");
         } else {
-            NotificationUtil.sendNotification(project, "File already added");
+            NotificationUtil.sendNotification(project, "File already added or no file selected");
         }
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
+        e.getPresentation().setEnabledAndVisible(file != null && !file.isDirectory());
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
+
+    @Override
+    public boolean isDumbAware() {
+        return true;
     }
 }
