a331f9c6d361489566333a707638732026845510
Stephan Janssen <sja@devoxx.com>
2024-08-14 20:30:20 +0200

    Feat #209 Upgraded to LangChain4j v0.33.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,14 +7,15 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.langchain4j:langchain4j:0.32.0")
-    implementation("dev.langchain4j:langchain4j-ollama:0.32.0")
-    implementation("dev.langchain4j:langchain4j-local-ai:0.32.0")
-    implementation("dev.langchain4j:langchain4j-open-ai:0.32.0")
-    implementation("dev.langchain4j:langchain4j-anthropic:0.32.0")
-    implementation("dev.langchain4j:langchain4j-mistral-ai:0.32.0")
-    implementation("dev.langchain4j:langchain4j-web-search-engine-google-custom:0.32.0")
-    implementation("dev.langchain4j:langchain4j-web-search-engine-tavily:0.32.0")
+    val lg4j_version = "0.33.0"
+    implementation("dev.langchain4j:langchain4j:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-ollama:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-local-ai:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-open-ai:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-anthropic:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-mistral-ai:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-web-search-engine-google-custom:$lg4j_version")
+    implementation("dev.langchain4j:langchain4j-web-search-engine-tavily:$lg4j_version")
 
     implementation("org.commonmark:commonmark:0.22.0")
 
