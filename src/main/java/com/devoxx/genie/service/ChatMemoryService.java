6e81d05b39340bf1d5c9810d1c81f458ffba3e9f
Stephan Janssen <sja@devoxx.com>
2024-09-05 09:41:49 +0200

    Fix #274 + Load history conversation into chat memory

--- src/main/java/com/devoxx/genie/service/ChatMemoryService.java
+++ src/main/java/com/devoxx/genie/service/ChatMemoryService.java
@@ -1,10 +1,14 @@
 package com.devoxx.genie.service;
+import com.devoxx.genie.model.conversation.Conversation;
 import com.devoxx.genie.model.request.ChatMessageContext;
 import com.devoxx.genie.ui.listener.ChatMemorySizeListener;
 import com.devoxx.genie.ui.topic.AppTopics;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.jetbrains.annotations.NotNull;
@@ -15,15 +19,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatMemoryService implements ChatMemorySizeListener {
 
-    private final Map<Project, MessageWindowChatMemory> projectMemories = new ConcurrentHashMap<>();
+    private final Map<String, MessageWindowChatMemory> projectMemories = new ConcurrentHashMap<>();
     private final InMemoryChatMemoryStore inMemoryChatMemoryStore = new InMemoryChatMemoryStore();
 
     public static ChatMemoryService getInstance() {
         return ApplicationManager.getApplication().getService(ChatMemoryService.class);
     }
 
-    public void init(Project project) {
-        createChatMemory(project, DevoxxGenieSettingsServiceProvider.getInstance().getChatMemorySize());
+    public void init(@NotNull Project project) {
+        createChatMemory(project.getLocationHash(), DevoxxGenieSettingsServiceProvider.getInstance().getChatMemorySize());
         createChangeListener();
     }
 
@@ -33,38 +37,40 @@ public class ChatMemoryService implements ChatMemorySizeListener {
             .subscribe(AppTopics.CHAT_MEMORY_SIZE_TOPIC, this);
     }
 
-    public void clear(Project project) {
-        projectMemories.get(project).clear();
+    public void clear(@NotNull Project project) {
+        projectMemories.get(project.getLocationHash()).clear();
     }
 
-    public void add(Project project, ChatMessage chatMessage) {
-        projectMemories.get(project).add(chatMessage);
+    public void add(@NotNull Project project, ChatMessage chatMessage) {
+        projectMemories.get(project.getLocationHash()).add(chatMessage);
     }
 
     public void remove(@NotNull ChatMessageContext chatMessageContext) {
         Project project = chatMessageContext.getProject();
-        List<ChatMessage> messages = projectMemories.get(project).messages();
+        List<ChatMessage> messages = projectMemories.get(project.getLocationHash()).messages();
         messages.remove(chatMessageContext.getAiMessage());
         messages.remove(chatMessageContext.getUserMessage());
-        projectMemories.get(project).clear();
+
+        // Remove the conversation from the storage service
+        projectMemories.get(project.getLocationHash()).clear();
         messages.forEach(message -> add(project, message));
     }
 
-    public void removeLast(Project project) {
-        List<ChatMessage> messages = projectMemories.get(project).messages();
+    public void removeLast(@NotNull Project project) {
+        List<ChatMessage> messages = projectMemories.get(project.getLocationHash()).messages();
         if (!messages.isEmpty()) {
             messages.remove(messages.size() - 1);
-            projectMemories.get(project).clear();
+            projectMemories.get(project.getLocationHash()).clear();
             messages.forEach(message -> add(project, message));
         }
     }
 
-    public List<ChatMessage> messages(Project project) {
-        return projectMemories.get(project).messages();
+    public List<ChatMessage> messages(@NotNull Project project) {
+        return projectMemories.get(project.getLocationHash()).messages();
     }
 
-    public boolean isEmpty(Project project) {
-        return projectMemories.get(project).messages().isEmpty();
+    public boolean isEmpty(@NotNull Project project) {
+        return projectMemories.get(project.getLocationHash()).messages().isEmpty();
     }
 
     @Override
@@ -72,12 +78,23 @@ public class ChatMemoryService implements ChatMemorySizeListener {
         projectMemories.forEach((project, memory) -> createChatMemory(project, chatMemorySize));
     }
 
-    private void createChatMemory(Project project, int chatMemorySize) {
+    private void createChatMemory(@NotNull String projectHash, int chatMemorySize) {
         MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder()
-            .id("devoxxgenie-" + project.getLocationHash())
+            .id("devoxxgenie-" + projectHash)
             .chatMemoryStore(inMemoryChatMemoryStore)
             .maxMessages(chatMemorySize)
             .build();
-        projectMemories.put(project, chatMemory);
+        projectMemories.put(projectHash, chatMemory);
+    }
+
+    public void restoreConversation(@NotNull Project project, @NotNull Conversation conversation) {
+        clear(project);
+        for (com.devoxx.genie.model.conversation.ChatMessage message : conversation.getMessages()) {
+            if (message.isUser()) {
+                add(project, new UserMessage(message.getContent()));
+            } else {
+                add(project,  new AiMessage(message.getContent()));
+            }
+        }
     }
 }
