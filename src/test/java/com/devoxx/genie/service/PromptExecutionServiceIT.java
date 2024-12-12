829d2fc101852cb5e4fdff571c38fb567b600512
samir <samir.kerr@gmail.com>
2024-12-12 12:01:24 +0100

    feat(): fixing compilation error on PromptExecutionServiceIT

--- src/test/java/com/devoxx/genie/service/PromptExecutionServiceIT.java
+++ src/test/java/com/devoxx/genie/service/PromptExecutionServiceIT.java
@@ -141,7 +141,8 @@ public class PromptExecutionServiceIT extends AbstractLightPlatformTestCase {
                 .apiKey(dotenv.get("MISTRAL_API_KEY"))
                 .modelName(languageModel.getModelName())
                 .build();
-            case DeepInfra -> OpenAiChatModel.builder()
+            case DeepInfra
+                    -> OpenAiChatModel.builder()
                 .baseUrl("https://api.deepinfra.com/v1/openai")
                 .apiKey(dotenv.get("DEEPINFRA_API_KEY"))
                 .modelName(languageModel.getModelName())
