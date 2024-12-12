8bdd1fa6a6dde2f9d9394a53c5585344c4b4fc29
Stephan Janssen <sja@devoxx.com>
2024-12-12 11:41:38 +0100

    Corrected integration test

--- src/main/java/com/devoxx/genie/controller/listener/ProjectContextListener.java
+++
@@ -1,8 +0,0 @@
-package com.devoxx.genie.controller.listener;
-
-public interface ProjectContextListener {
-
-    void onProjectContextAdded(int tokenCount, int tokenLimit);
-
-    void onProjectContextRemoved();
-}
