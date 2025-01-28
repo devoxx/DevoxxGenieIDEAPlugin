ff35d261d2ceff1f939874a6c0e276b7bedc6578
Stephan Janssen <sja@devoxx.com>
2025-01-28 09:59:13 +0100

    Fix #168 Copy & Paste now keeps the newlines of the copied code

--- src/main/java/com/devoxx/genie/ui/renderer/CodeBlockNodeRenderer.java
+++ src/main/java/com/devoxx/genie/ui/renderer/CodeBlockNodeRenderer.java
@@ -4,9 +4,6 @@ import com.devoxx.genie.ui.util.LanguageGuesser;
 import com.intellij.lang.Language;
 import com.intellij.lang.documentation.DocumentationSettings;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.HighlighterColors;
-import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.editor.richcopy.HtmlSyntaxInfoUtil;
 import com.intellij.openapi.fileTypes.PlainTextLanguage;
 import com.intellij.openapi.project.Project;
@@ -20,7 +17,8 @@ import org.commonmark.renderer.html.HtmlNodeRendererContext;
 import org.commonmark.renderer.html.HtmlWriter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -38,17 +36,14 @@ public class CodeBlockNodeRenderer implements NodeRenderer {
     private final Project project;
     private final HtmlWriter htmlOutputWriter;
 
-    public CodeBlockNodeRenderer(Project project, @NotNull
-    HtmlNodeRendererContext context) {
+    public CodeBlockNodeRenderer(Project project, @NotNull HtmlNodeRendererContext context) {
         this.project = project;
         this.htmlOutputWriter = context.getWriter();
     }
 
     @Override
     public Set<Class<? extends Node>> getNodeTypes() {
-        return Set.of(
-            IndentedCodeBlock.class, FencedCodeBlock.class, Code.class
-        );
+        return Set.of(IndentedCodeBlock.class, FencedCodeBlock.class, Code.class);
     }
 
     @Override
@@ -59,8 +54,6 @@ public class CodeBlockNodeRenderer implements NodeRenderer {
             renderNode(fencedCodeBlock.getLiteral(), fencedCodeBlock.getInfo(), true);
         } else if (node instanceof Code code) {
             renderNode(code.getLiteral(), false);
-        } else {
-            System.err.println("Unknown node type: " + node);
         }
     }
 
@@ -68,21 +61,49 @@ public class CodeBlockNodeRenderer implements NodeRenderer {
         htmlOutputWriter.line();
 
         if (block) {
-            htmlOutputWriter.tag("pre");
+            // Style to ensure proper whitespace handling
+            Map<String, String> preStyle = new HashMap<>();
+            preStyle.put("style", "-webkit-user-select: text; user-select: text; white-space: pre !important;");
+            htmlOutputWriter.tag("pre", preStyle);
         }
 
-        htmlOutputWriter.tag("code", Collections.singletonMap("style", "font-size:14pt"));
+        // Critical: we need to handle newlines properly in the code tag
+        Map<String, String> codeStyle = new HashMap<>();
+        codeStyle.put("style", "font-size:14pt; white-space: pre !important; -webkit-user-select: text; user-select: text;");
+        htmlOutputWriter.tag("code", codeStyle);
 
         HighlightingMode highlightingMode = determineHighlightingMode(block);
+        Language language = LanguageGuesser.guessLanguage(info);
+        if (language == null) {
+            language = PlainTextLanguage.INSTANCE;
+        }
 
-        htmlOutputWriter.raw(
-            appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
-                highlightingMode,
-                project,
-                LanguageGuesser.guessLanguage(info) != null ? LanguageGuesser.guessLanguage(info) : PlainTextLanguage.INSTANCE,
-                codeSnippet
-            )
-        );
+        // Get highlighted code
+        StringBuilder highlightedCode = new StringBuilder();
+        Language finalLanguage = language;
+        ApplicationManager.getApplication().runReadAction(() -> {
+            if (highlightingMode == HighlightingMode.SEMANTIC_HIGHLIGHTING) {
+                HtmlSyntaxInfoUtil.appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
+                        highlightedCode,
+                        project,
+                        finalLanguage,
+                        codeSnippet,
+                        false,
+                        DocumentationSettings.getHighlightingSaturation(true)
+                );
+            } else {
+                highlightedCode.append(StringUtil.escapeXmlEntities(codeSnippet));
+            }
+        });
+
+        // Process the code to ensure proper line breaks
+        String processedCode = highlightedCode.toString()
+                .replace("&#32;", " ")                              // Convert space entities to spaces
+                .replaceAll("</span><span[^>]*><br></span>", "\n") // Convert br tags to newlines
+                .replaceAll("<br>\\s*", "\n")                      // Handle any remaining br tags
+                .replaceAll("\\n", "</span>\n<span>");             // Ensure spans wrap around newlines
+
+        htmlOutputWriter.raw(processedCode);
 
         htmlOutputWriter.tag("/code");
 
@@ -109,32 +130,5 @@ public class CodeBlockNodeRenderer implements NodeRenderer {
         } else {
             return HighlightingMode.INLINE_HIGHLIGHTING;
         }
-    }
-
-    private @NotNull String appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
-        HighlightingMode highlightingMode, Project project, Language language, String codeSnippet) {
-        StringBuilder highlightedAndEncodedAsHtmlCodeSnippet = new StringBuilder();
-        ApplicationManager.getApplication().runReadAction(() -> {
-            if (highlightingMode == HighlightingMode.SEMANTIC_HIGHLIGHTING) {
-                HtmlSyntaxInfoUtil.appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
-                    highlightedAndEncodedAsHtmlCodeSnippet,
-                    project,
-                    language,
-                    codeSnippet,
-                    false,
-                    DocumentationSettings.getHighlightingSaturation(true)
-                );
-            } else {
-                highlightedAndEncodedAsHtmlCodeSnippet.append(StringUtil.escapeXmlEntities(codeSnippet));
-            }
-        });
-
-        if (highlightingMode != HighlightingMode.NO_HIGHLIGHTING) {
-            TextAttributes codeAttributes =
-                EditorColorsManager.getInstance().getGlobalScheme().getAttributes(HighlighterColors.TEXT).clone();
-            codeAttributes.setBackgroundColor(null);
-        }
-
-        return highlightedAndEncodedAsHtmlCodeSnippet.toString();
     }
 }
